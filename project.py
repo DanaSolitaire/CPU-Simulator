5c721f79431dd3ff40aa39e3274c4a6afe114daf
emlambda <110039232+emlambda@users.noreply.github.com>
2024-02-27 15:31:07 -0500

    updated project

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-import numpy as np
+import math
 
 class Rand(object):
     def __init__(self, seed):
@@ -14,15 +14,63 @@ class Rand(object):
     def drand(self):
         return self.next() / 2**48
 
+def next_exp(rand,lamb):
+    x = math.log(1-rand.drand())/(-lamb)
+    return x
+def processes(n, n_cpu, seed, lamb, ceil):
+    """_summary_
+
+    Args:
+        n (int): number of processes
+        n_cpu (int): number of processes that are CPU-bound
+        seed (int): seed for the pseudorandom number sequence
+        lamb (float): rate parameter of exponential distribution
+        ceil (int): upper bound for valid pseudo-random numbers
+    """
+    rand = Rand(seed)
+    rand.srand(seed)
+    alph='ABCDEFGHIJKLMNOPQRSTTUVQXYZ'
+    
+    for process in range(n):
+        
+        num = next_exp(rand, lamb)
+        
+        while (num < 0) | (num > ceil):
+            num = next_exp(rand, lamb)
+            
+        arrival_time = math.floor(num)
+        bursts = math.ceil(rand.drand()*64)
+        
+        print(f"I/O-bound process {alph[process]}: arrival time {arrival_time}ms; {bursts} CPU bursts:")
+        
+        for burst in range(bursts):
+            
+            cpu_burst_time = math.ceil(next_exp(rand, lamb))
+            io_burst_time = math.ceil(next_exp(rand, lamb)*10)
+            
+            if process >= (n-n_cpu):
+                cpu_burst_time *= 4
+                io_burst_time /= 8
+            if burst == bursts-1:
+                terminal_out(cpu_burst_time)
+            else:
+                terminal_out(cpu_burst_time,io_burst_time)
+    
+def terminal_out(cpu_burst, io_burst=None):
+    if not io_burst:
+        print(f"--> CPU burst {cpu_burst}ms")
+    else:
+        print(f"--> CPU burst {cpu_burst}ms --> I/O burst {io_burst}ms")
 
 if __name__ == '__main__':
     
     assert len(sys.argv) == 6, "Incorrect number of arguments"
     
-    n = sys.argv[1]
-    n_cpu = sys.argv[2]
-    seed = sys.argv[3]
-    lamb = sys.argv[4]
-    ceil = sys.argv[5]
+    n = int(sys.argv[1])
+    n_cpu = int(sys.argv[2])
+    seed = int(sys.argv[3])
+    lamb = float(sys.argv[4])
+    ceil = int(sys.argv[5])
+    processes(n,n_cpu,seed,lamb,ceil)
     
     
